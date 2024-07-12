2d899cab3f52a79aa95fec56d7d8a0578169861a
Matthias Hammelehle <mahammelehle@outlook.de>
2024-07-12 20:40:39 +0200

    added skill section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,3 +1,4 @@
+import { SkillsComponent } from './skills/skills.component';
 import { Component } from '@angular/core';
 import { ProfileTopSectionComponent } from './profile-top-section/profile-top-section.component';
 import { AboutMeComponent } from './about-me/about-me.component';
@@ -5,7 +6,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [ProfileTopSectionComponent, AboutMeComponent],
+  imports: [ProfileTopSectionComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
