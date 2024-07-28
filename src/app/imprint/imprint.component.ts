7f333401a9298150602d6269c53f9cc92249caa4
Matthias Hammelehle <mahammelehle@outlook.de>
2024-07-28 22:13:37 +0200

    changed some style in mobile view

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -3,14 +3,15 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AppComponent } from "../app.component";
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterModule, AppComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterModule, AppComponent, RouterOutlet, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
 }
